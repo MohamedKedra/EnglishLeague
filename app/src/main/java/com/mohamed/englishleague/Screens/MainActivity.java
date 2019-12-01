6305cfdaa1394f692a6089612a9a2c6ea51b6ee8
Mohamed Ali Kedra <mohamed.a.kedra@gmail.com>
2019-12-02 00:18:14 +0200

    update 3#
    pagination update

--- app/src/main/java/com/mohamed/englishleague/Screens/MainActivity.java
+++ app/src/main/java/com/mohamed/englishleague/Screens/MainActivity.java
@@ -1,38 +1,38 @@
 package com.mohamed.englishleague.Screens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
-import com.mohamed.englishleague.API.Client;
-import com.mohamed.englishleague.API.LeagueService;
 import com.mohamed.englishleague.Adapters.TeamAdapter;
 import com.mohamed.englishleague.Models.Team;
-import com.mohamed.englishleague.Utils.Constants;
-import com.mohamed.englishleague.Models.LeagueResponse;
 import com.mohamed.englishleague.R;
 import com.mohamed.englishleague.ViewModels.MainViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     TeamAdapter adapter;
     ProgressBar progressBar;
-    RecyclerView recyclerView;
+    RecyclerView teamsRecycler;
     RecyclerView.LayoutManager layoutManager;
     MainViewModel mainViewModel;
+    FrameLayout loading;
+    List<Team> teamsList;
+    static int page = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,29 +41,92 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
         displayAllTeams();
+        test();
     }
 
-    public void initViews(){
-        recyclerView = findViewById(R.id.rv_teams);
+    public void initViews() {
+        teamsRecycler = findViewById(R.id.rv_teams);
         progressBar = findViewById(R.id.pb_progressbar);
-        layoutManager = new GridLayoutManager(this,2);
+        loading = findViewById(R.id.lay_loading);
+        layoutManager = new GridLayoutManager(this, 2);
     }
 
-    public void displayAllTeams(){
+    public void displayAllTeams() {
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        mainViewModel.getAllTeams().observe(this,observer);
+        mainViewModel.getAllTeams().observe(this, observer);
     }
 
     Observer<List<Team>> observer = new Observer<List<Team>>() {
         @Override
         public void onChanged(List<Team> teams) {
             progressBar.setVisibility(View.GONE);
-            if (teams != null){
-                adapter = new TeamAdapter(teams,MainActivity.this);
-                recyclerView.setAdapter(adapter);
-                recyclerView.setLayoutManager(layoutManager);
+            if (teams != null) {
+                teamsList = teams;
+                adapter = new TeamAdapter(getListFromPage(teamsList,page), MainActivity.this);
+                page++;
+                teamsRecycler.setAdapter(adapter);
+                teamsRecycler.setLayoutManager(layoutManager);
             }
         }
     };
 
+    public boolean isLastItemDisplayed() {
+
+
+        if (teamsRecycler.getAdapter().getItemCount() != 0) {
+
+            int lastItemPosition = ((LinearLayoutManager) teamsRecycler.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+            System.out.println("scroll "+lastItemPosition+ " and "+teamsRecycler.getAdapter().getItemCount());
+            if (lastItemPosition != RecyclerView.NO_POSITION && lastItemPosition == teamsRecycler.getAdapter().getItemCount() - 1) {
+                System.out.println("scroll ok");
+                return true;
+            }
+            System.out.println("scroll no");
+        }
+        return false;
+    }
+
+    public void test() {
+        teamsRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (isLastItemDisplayed()){
+                    System.out.println("scroll page "+page);
+                    loading.setVisibility(View.VISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            loading.setVisibility(View.GONE);
+                            if (page < 5){
+                                adapter = new TeamAdapter(getListFromPage(teamsList,page),MainActivity.this);
+                                teamsRecycler.setAdapter(adapter);
+                                teamsRecycler.setLayoutManager(layoutManager);
+                                teamsRecycler.scrollToPosition((page - 1) * 6 );
+                                page++;
+                                System.out.println("scroll more");
+                            }
+                        }
+                    },1000);
+                }
+            }
+        });
+    }
+
+    public List<Team> getListFromPage(List<Team> teams ,int page){
+
+        List<Team> newList = new ArrayList<>();
+        System.out.println("scroll "+page);
+        int index = teams.size() > page * 6 ? page * 6: teams.size();
+        for (int i = 0; i < index; i++ ){
+            newList.add(teams.get(i));
+        }
+        return newList;
+    }
 }
